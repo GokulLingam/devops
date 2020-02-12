b4503bcf21f1af6cc58d1d0cadd334b1156ea249
gokul <gokullingam6@gmail.com>
2020-02-13 01:42:00 +0530

    added bootstrap and jquery demendencies

---
+++ src/main/java/com/devops/controller/helloworldcontroller.java
@@ -0,0 +1,14 @@
+package com.devops.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class helloworldcontroller {
+	 @RequestMapping("/")
+ public String sayhello() {
+	
+	 return "index";
+ }
+
+}
